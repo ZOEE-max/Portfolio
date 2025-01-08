f2901d314e7930c43eeaf4e87fd91b179c9e3c02
ZOEE-max <zoewyke@gmail.com>
2025-01-08 01:38:22 +0100

    nu det sidste

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
+// Forsinkelse til min tekst på index
 window.addEventListener("load", function () {
   const content = document.querySelector(".content");
   content.style.opacity = 1;
 });
 
 // Zooom til mine billeder-------------
-// Find billedet
+//
 const image = document.querySelector(".zoomable");
 
-// Tilføj klik-event til billedet
 image.addEventListener("click", () => {
-  // Tjek om billedet allerede er zoomet
   if (image.classList.contains("zoomed")) {
-    image.classList.remove("zoomed"); // Fjern zoom
+    image.classList.remove("zoomed");
   } else {
-    image.classList.add("zoomed"); // Tilføj zoom
+    image.classList.add("zoomed");
   }
 });
