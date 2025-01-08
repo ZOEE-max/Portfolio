b6a681832472fd246e329776427d314231ba2815
ZOEE-max <zoewyke@gmail.com>
2025-01-08 01:33:27 +0100

    sidstee

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ window.addEventListener("load", function () {
   const content = document.querySelector(".content");
   content.style.opacity = 1;
 });
+
+// Zooom til mine billeder-------------
+// Find billedet
+const image = document.querySelector(".zoomable");
+
+// Tilføj klik-event til billedet
+image.addEventListener("click", () => {
+  // Tjek om billedet allerede er zoomet
+  if (image.classList.contains("zoomed")) {
+    image.classList.remove("zoomed"); // Fjern zoom
+  } else {
+    image.classList.add("zoomed"); // Tilføj zoom
+  }
+});
